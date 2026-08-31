846f342dd21f704a743c7d392b78d4f2eca6905c
Pgr0708 <parthgohil0708@gmail.com>
2026-08-31 17:05:43 +0530

    Initial Commit

---
+++ SnapShort/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapShort
+//
+//  Created by Minaxi on 31/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapShort/SnapShortApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapShortApp.swift
+//  SnapShort
+//
+//  Created by Minaxi on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapShortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
